d2b0c3c68eb432cb06246d49ce875d3433e52c05
rajat bhatt <bettertryingtobecome@gmail.com>
2024-07-28 16:36:26 +0530

    Updates

---
+++ precache-manifest.1a62e07cdae29b219ed174cf81af95a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6a31998b7b7e550b4ea65f306a6657",
+    "url": "/my-first-project-textutils/index.html"
+  },
+  {
+    "revision": "29999972fd3a4b2c9546",
+    "url": "/my-first-project-textutils/static/css/main.db5c7e05.chunk.css"
+  },
+  {
+    "revision": "0da3a3a7caac8e2de5c7",
+    "url": "/my-first-project-textutils/static/js/2.a5b0c296.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/my-first-project-textutils/static/js/2.a5b0c296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/my-first-project-textutils/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "29999972fd3a4b2c9546",
+    "url": "/my-first-project-textutils/static/js/main.00600a84.chunk.js"
+  },
+  {
+    "revision": "3c0702fa8c88bbfc3f0d",
+    "url": "/my-first-project-textutils/static/js/runtime-main.dfa8d79e.js"
+  }
+]);
